c6d0a61b64979425c0ed0a58cd196353c9724421
lineilx <141544795+lineilx@users.noreply.github.com>
2023-08-26 18:55:38 +0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+        region = "us-east-1"
+  }
+  resource "aws_vpc" "test11" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "test11"
+    vpctest = "123"
+  }
+}
+    
+  
